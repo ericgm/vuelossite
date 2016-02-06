ea62a6e7701ef94c4429ae2517b2d294d2ff7040
ericgm <19eric86@gmail.com>
2016-02-06 11:34:35 -0500

    edit usuario

--- Install.ps1
+++ Install.ps1
@@ -1,1 +1,1 @@
-cinst VuelosSite -source .\Build -f -y -packageParameters '"/Destination:C:\inetpub\wwwroot\VuelosSite /AppName:Default Web Site\VuelosSite /UserName:eric /Password:cruiserprado "'
+cinst VuelosSite -source .\Build -f -y -packageParameters '"/Destination:C:\inetpub\wwwroot\VuelosSite /AppName:Default Web Site\VuelosSite /UserName:usuario /Password:clave "'
